d35eaf6e8711c754cfe9344405baa9391d58f97f
marco <marco-pol@hotmail.it>
2021-09-09 01:05:20 +0200

    Mobile-nav animation

--- fire-blog/src/components/Navigation.vue
+++ fire-blog/src/components/Navigation.vue
@@ -144,5 +144,22 @@ header {
       color: #ffffff;
     }
   }
+
+  .mobile-nav-enter-active,
+  .mobile-nav-leave-active {
+    transition: all 1s ease;
+  }
+
+  .mobile-nav-enter {
+    transform: translateX(-250px);
+  }
+
+  .mobile-nav-enter-to {
+    transform: translateX(0);
+  }
+
+  .mobile-nav-leave-to {
+    transform: translateX(-250px);
+  }
 }
 </style>
