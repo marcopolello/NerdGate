f747b902988ea6929597a6187822da3dccb39da8
marco <marco-pol@hotmail.it>
2021-10-07 17:23:20 +0200

    unvers

---
+++ fire-blog/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ fire-blog/src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
